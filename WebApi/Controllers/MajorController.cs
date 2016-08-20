980617a7be74f94e4e49626df93d9565bbbbaad2
John Paul Azar <jazar@ucsd.edu>
2016-08-20 14:52:49 -0700

    added major controller and tests

---
+++ WebApi/Controllers/MajorController.cs
@@ -0,0 +1,74 @@
+﻿namespace WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using POCO;
+    using IRepository;
+    using Repository;
+    using Service;
+
+    public class MajorController : ApiController
+    {
+
+        [HttpGet]
+        public List<Major> GetMajorList(ref List<string> errors)
+        {
+            var repository = new MajorRepository();
+            var service = new MajorService(repository);
+            return service.GetMajorList(ref errors);
+        }
+
+        [HttpGet]
+        public Major GetMajor(string id, ref List<string> errors)
+        {
+            var repository = new MajorRepository();
+            var service = new MajorService(repository);
+            return service.GetMajor(id, ref errors);
+        }
+
+        [HttpPost]
+        public string InsertMajor(Major major, ref List<string> errors)
+        {
+            var repository = new MajorRepository();
+            var service = new MajorService(repository);
+            service.InsertMajor(major, ref errors);
+            if (errors.Count == 0)
+            {
+                return "ok";
+            }
+
+            return "error";
+        }
+
+        [HttpPost]
+        public string UpdateMajor(Major major, ref List<string> errors)
+        {
+            var repository = new MajorRepository();
+            var service = new MajorService(repository);
+            service.UpdateMajor(major, ref errors);
+
+            if (errors.Count == 0)
+            {
+                return "ok";
+            }
+
+            return "error";
+        }
+
+        [HttpPost]
+        public string DeleteMajor(string id)
+        {
+            var errors = new List<string>();
+            var repository = new MajorRepository();
+            var service = new MajorService(repository);
+            service.DeleteMajor(id, ref errors);
+
+            if (errors.Count == 0)
+            {
+                return "ok";
+            }
+
+            return "error";
+        }
+    }
+}
---
+++ WebApiTest/MajorControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POCO;
+using WebApi.Controllers;
+using System.Collections.Generic;
+
+namespace WebApiTest
+{
+    [TestClass]
+    public class MajorControllerTest
+    {
+
+        public Major createMajor()
+        {
+            var major = new Major
+            {
+                Id = 1,
+                FullName = "Computer Science and Engineering",
+                ShorthandName = "CSE",
+                Description = "A major for people who want to be computer programmers and engineers.",
+            };
+            return major;
+        }
+
+        public Major modifyMajor()
+        {
+            var major = new Major
+            {
+                Id = 1,
+                FullName = "Computer Science and Engineering!",
+                ShorthandName = "CSE",
+                Description = "A major for people who want to be computer programmers and engineers!",
+            };
+            return major;
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InsertMajorSuccessTest()
+        {
+            var errors = new List<string>();
+            var majorController = new MajorController();
+            var success = majorController.InsertMajor(createMajor(), ref errors);
+            Assert.AreEqual("ok", success);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetMajorSuccessTest()
+        {
+            var errors = new List<string>();
+            var majorController = new MajorController();
+            var major = majorController.GetMajor("1", ref errors);
+            Assert.AreEqual(0, errors.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UpdatetMajorSuccessTest()
+        {
+            var errors = new List<string>();
+            var majorController = new MajorController();
+            var success = majorController.UpdateMajor(modifyMajor(), ref errors);
+            Assert.AreEqual("ok", success);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DeleteMajorSuccessTest()
+        {
+
+            var majorController = new MajorController();
+            var success = majorController.DeleteMajor("1");
+            Assert.AreEqual("ok", success);
+        }
+    }
+}
