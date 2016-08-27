1b04cef2195a96d4c246c4fe5142747ca1f133c1
John Paul Azar <jazar@ucsd.edu>
2016-08-26 19:35:06 -0700

    if this doesn't work I will shank someone

---
+++ MVC/Scripts/136Libraries/Models/ScheduleListModel.js
@@ -0,0 +1,36 @@
+﻿function ScheduleListModel() {
+
+    this.Load = function (callback) {
+  /**      $.ajax({
+            url: "http://localhost:9393/Api/Schedule/GetScheduleList",
+            data: "",
+            dataType: "json",
+            success: function (courseListData) {
+                callback(courseListData);
+            },
+            error: function () {
+                alert('Error while loading course list.  Is your service layer running?');
+            }
+        });  We do this for part 5 **/
+        var scheduleList = [];
+        scheduleList.push({
+            Year: "2011",
+            Quarter: "Spring",
+            Session: "A",
+            Course: "Introduction to Java",
+            Instructor: "John Smith",
+            Day: "MW",
+            Time: "9am-12pm"
+        });
+        scheduleList.push({
+            Year: "2012",
+            Quarter: "Winter",
+            Session: "B",
+            Course: "Data Structures",
+            Instructor: "Some Instructor",
+            Day: "TuThurs",
+            Time: "10am-11:50pm"
+        });
+        callback(scheduleList);
+    };
+}
---
+++ MVC/Scripts/136Libraries/ViewModels/ScheduleListViewModel.js
@@ -0,0 +1,30 @@
+﻿function ScheduleListViewModel() {
+    this.Load = function () {
+        var scheduleListModelObj = new ScheduleListModel();
+
+        // Because the Load() is a async call (asynchronous), we'll need to use
+        // the callback approach to handle the data after data is loaded.
+        scheduleListModelObj.Load(function (scheduleListData) {
+
+            // courseList - presentation layer model retrieved from /schedule/GetscheduleList route.
+            // scheduleListViewModel - view model for the html content
+            var scheduleListViewModel = new Array();
+
+            // DTO from the JSON model to the view model. In this case, scheduleListViewModel doesn't need the "id" attribute
+            for (var i = 0; i < scheduleListData.length; i++) {
+                scheduleListViewModel[i] = {
+                    Year: scheduleListData[i].Year,
+                    Quarter: scheduleListData[i].Quarter,
+                    Session: scheduleListData[i].Session,
+                    Course: scheduleListData[i].Course,
+                    Instructor: scheduleListData[i].Instructor,
+                    Day: scheduleListData[i].Day,
+                    Time: scheduleListData[i].Time
+                };
+            }
+
+            // this is using knockoutjs to bind the viewModel and the view (Home/Index.cshtml)
+            ko.applyBindings({ viewModel: scheduleListViewModel }, document.getElementById("divScheduleListEdit"));
+        });
+    };
+}
