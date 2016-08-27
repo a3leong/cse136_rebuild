d9f9b403ba1bb232ffc9e0dbb2e390a1beb72668
Aaron Leong <a3leong@gmail.com>
2016-08-27 16:10:43 -0700

    Fixed file name not consistent with class name, added files that were untracked

---
+++ MVC/Scripts/136Libraries/Models/MajorModel.js
@@ -0,0 +1,52 @@
+﻿function MajorModel() {
+    this.Update = function (major, callback) {
+  /**      $.ajax({
+            async: asyncIndicator,
+            method: "POST",
+            url: "http://localhost:9393/Api/Major/InsertMajor",
+            data: major,
+            dataType: "json",
+            success: function (result) {
+                callback(result);
+            },
+            error: function () {
+                alert('Error while adding major.  Is your service layer running?');
+            }
+        }); **/
+        return "ok";
+    }
+
+    this.LoadMajor = function (id, callback) {
+        var Major = {
+            Id: 3,
+            FullName: "Critical Gender Studies",
+            ShorthandName: "CGS",
+            Description: "Study genders and write papers on it"
+        }
+        return Major;
+    }
+
+    this.LoadMajorList = function (callback) {
+  /**      $.ajax({
+            url: "http://localhost:9393/Api/Major/GetMajorList",
+            data: "",
+            dataType: "json",
+            success: function (courseListData) {
+                callback(courseListData);
+            },
+            error: function () {
+                alert('Error while loading course list.  Is your service layer running?');
+            }
+        });  We do this for part 5 **/
+        var MajorList = [];
+        MajorList.push({
+            FullName: "Computer Science",
+            Description: "Learn how to hello world."
+        });
+        MajorList.push({
+            FullName: "Computer Science2",
+            Description: "Learn how search Stack Overflow."
+        });
+        callback(MajorList);
+    };
+}
---
+++ MVC/Scripts/136Libraries/ViewModels/MajorViewModel.js
@@ -0,0 +1,63 @@
+﻿function MajorViewModel() {
+    var MajorModelObj = new MajorModel();
+
+    this.UpdateMajor = function (data) {
+        var model = {
+            Id: data.id(),
+            FullName: data.fullname(),
+            ShorthandName: data.shorthandname(),
+            Description: data.description()
+        }
+
+        MajorModelObj.Update(model, function (result) {
+            if (result == "ok") {
+                alert("Create student successful");
+            } else {
+                alert("Error occurred");
+            }
+        });
+    };
+
+    this.LoadMajor = function (id) {
+        MajorModelObj.LoadMajor(id, function (majorData) {
+            var majorViewModel = {
+                id: majorData.Id,
+                fullname: majorData.FullName,
+                shorthandname: majorData.ShorthandName,
+                description: majorData.Description,
+                update: function (data) {
+                    self.UpdateMajor(data);
+                }
+            };
+
+            // this is using knockoutjs to bind the viewModel and the view (Home/Index.cshtml)
+            ko.applyBindings({ viewModel: majorModelObj }, document.getElementById("divMajorEdit"));
+        });
+    };
+
+    this.LoadMajorList = function () {
+        var majorListModelObj = new MajorListModel();
+
+        // Because the Load() is a async call (asynchronous), we'll need to use
+        // the callback approach to handle the data after data is loaded.
+        majorListModelObj.LoadMajorList(function (majorListData) {
+
+            // courseList - presentation layer model retrieved from /Major/GetMajorList route.
+            // majorListViewModel - view model for the html content
+            var majorListViewModel = new Array();
+
+            // DTO from the JSON model to the view model. In this case, majorListViewModel doesn't need the "id" attribute
+            for (var i = 0; i < majorListData.length; i++) {
+                majorListViewModel[i] = {
+                                            id: majorListData[i].Id,
+                                            fullname: majorListData[i].FullName,
+                                            shorthandname: majorData.ShorthandName,
+                                            description: majorListData[i].Description
+                                        };
+            }
+
+            // this is using knockoutjs to bind the viewModel and the view (Home/Index.cshtml)
+            ko.applyBindings({ viewModel: majorListViewModel }, document.getElementById("divMajorList"));
+        });
+    };
+}
